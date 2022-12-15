1815472aa40298f70efbde5ad50ca71140dfec7d
Stefan Bodrozic <stefanbodrozic22@gmail.com>
2022-12-15 23:10:26 +0100

    update

---
+++ src/helpers/roles.js
@@ -0,0 +1,20 @@
+const roles = [
+  {
+    id: 1,
+    name: "Admin",
+  },
+  {
+    id: 2,
+    name: "Customer",
+  },
+  {
+    id: 3,
+    name: "Seller",
+  },
+  {
+    id: 4,
+    name: "Deliverer",
+  },
+];
+
+export default roles;
